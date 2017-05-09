f7f38289b30e8c9033f9ccb5293606e8646d2b22
Igor Yaitsky <yaickiu@gmail.com>
2017-05-09 18:57:37 +0300

    shopping list with react-animation

--- app/App.js
+++ app/App.js
@@ -1,12 +1,81 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-class App extends Component {
-  render(){
+class AnimatedShoppingList extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          name: 'Milk'
+        }, {
+          id: 2,
+          name: 'Yogurt'
+        }, {
+          id: 3,
+          name: 'Orange Juice'
+        }
+      ]
+    }
+  }
+
+  handleChange(event) {
+    if (event.key === 'Enter') {
+      let newItem = {
+        id: Date.now(),
+        name: event.target.value
+      };
+      let newItems = this
+        .state
+        .items
+        .concat(newItem);
+      event.target.value = '';
+      this.setState({items: newItems});
+    }
+  }
+
+  handleRemove(i) {
+    var newItems = this.state.items;
+    newItems.splice(i, 1);
+    this.setState({items: newItems});
+  }
+
+  render() {
+    let shoppingItems = this
+      .state
+      .items
+      .map((item, i) => (
+        <div
+          key={item.id}
+          className="item"
+          onClick={this
+          .handleRemove
+          .bind(this, i)}>
+          {item.name}
+        </div>
+      ));
     return (
-      <h1>Hello World</h1>
+      <div>
+        <ReactCSSTransitionGroup
+          transitionName="animation"
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+          transitionAppear={true}
+          transitionAppearTimeout={300}>
+          {shoppingItems}
+        </ReactCSSTransitionGroup>
+        <input
+          type="text"
+          value={this.state.newItem}
+          onKeyDown={this
+          .handleChange
+          .bind(this)}/>
+      </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(
+  <AnimatedShoppingList/>, document.getElementById('root'));
